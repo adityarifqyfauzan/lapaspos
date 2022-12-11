45f2c4ae084ac23c791f0740bb3af8c62bc74de2
Aditya Rifqy Fauzan <adityarifqyfauzan@gmail.com>
2022-12-11 08:44:35 +0700

    feat(category): product category
    create context and modify some service

--- app/Providers/ContextProvider.php
+++ app/Providers/ContextProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Http\Context\Auth\AuthContext;
 use App\Http\Context\Auth\AuthContextInterface;
+use App\Http\Context\Category\CategoryContext;
+use App\Http\Context\Category\CategoryContextInterface;
 use Illuminate\Support\ServiceProvider;
 
 class ContextProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class ContextProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(AuthContextInterface::class, AuthContext::class);
+        $this->app->bind(CategoryContextInterface::class, CategoryContext::class);
     }
 
     /**
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repository\CategoryRepository;
 use App\Repository\LoginHistoryRepository;
 use App\Repository\UserRepository;
+use App\Services\CategoryService;
 use App\Services\LoginHistoryService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserRepository::class, UserService::class);
         $this->app->bind(LoginHistoryRepository::class, LoginHistoryService::class);
+        $this->app->bind(CategoryRepository::class, CategoryService::class);
     }
 
     /**
