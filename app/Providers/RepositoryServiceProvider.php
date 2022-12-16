6886809e9651a20408cf61431368dba97ee59693
Aditya Rifqy Fauzan <adityarifqyfauzan@gmail.com>
2022-12-16 13:00:41 +0700

    feat(auth): add role in login response

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Repository\LoginHistoryRepository;
+use App\Repository\RoleRepository;
 use App\Repository\UserRepository;
 use App\Services\LoginHistoryService;
+use App\Services\RoleService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserRepository::class, UserService::class);
         $this->app->bind(LoginHistoryRepository::class, LoginHistoryService::class);
+        $this->app->bind(RoleRepository::class, RoleService::class);
     }
 
     /**
